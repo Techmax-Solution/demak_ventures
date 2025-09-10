33456fa218a4c83bb774486522ce1982dc2d01f4
Richard Asamoah <richardasamoah284@gmail.com>
2025-09-10 11:12:18 +0000

    database-connection-setup

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -64,7 +64,7 @@ const productSchema = new mongoose.Schema({
         size: {
             type: String,
             required: true,
-            enum: ['XS', 'S', 'M', 'L', 'XL', 'XXL', '28', '30', '32', '34', '36', '38', '40', '42']
+            enum: ['XS', 'S', 'M', 'L', 'XL', 'XXL', '28', '30', '32', '34', '36', '38', '40', '42', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', 'One Size']
         },
         quantity: {
             type: Number,
